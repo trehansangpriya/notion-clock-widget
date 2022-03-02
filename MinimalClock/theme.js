5347393947ff2f492abc2da784e03f2a625b269e
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-03-02 12:09:20 +0530

    query control and am/pm fix

--- MinimalClock/theme.js
+++ MinimalClock/theme.js
@@ -1,3 +1,17 @@
+//url query string
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+let value = params.theme; // "some_value"
+console.log(value);
+// set initial theme
+if (value == "dark") {
+    document.body.className = "dark";
+}
+if (value == "light") {
+    document.body.className = "light";
+}
 // toggle light/dark mode
 function toggleTheme() {
     var body = document.getElementsByTagName("body")[0];
